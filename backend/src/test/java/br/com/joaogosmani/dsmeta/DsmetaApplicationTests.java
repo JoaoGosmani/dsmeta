537c27900eaa2f32a078423d2c2a63fd525705e9
Joao Gosmani <joaogosmani@gmail.com>
2022-07-15 14:39:04 -0300

    Project created

---
+++ backend/src/test/java/br/com/joaogosmani/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
